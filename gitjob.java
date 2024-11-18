2dbccefa98157a134eef9f50b57789eb52b1c73d
lourbirds <lourencecortez@gmail.com>
2024-11-18 18:18:07 +0800

    Add files via upload

---
+++ gitjob.java
@@ -0,0 +1,11 @@
+package default1;
+
+public class gitjob {
+	public static void main(String[] args) {
+		msg();
+	}
+	static void msg() {
+		System.out.println(" Good day, How may I help you?");
+		System.out.println(" hrllo world");
+	}
+}
